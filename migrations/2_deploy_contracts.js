e68d89de7873812b99a1d081db3de382d17c939a
Louie <alouiseq@gmail.com>
2022-07-17 07:51:08 -0700

    add token contract and transfer balance to ethswap

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,13 @@
+const Token = artifacts.require("Token");
 const EthSwap = artifacts.require("EthSwap");
 
-module.exports = function(deployer) {
-  deployer.deploy(EthSwap);
+module.exports = async function(deployer) {
+  await deployer.deploy(Token);
+  token = await Token.deployed();
+
+  await deployer.deploy(EthSwap);
+  ethSwap = await EthSwap.deployed();
+
+  await token.transfer(ethSwap.address, '1000000000000000000000000');
 };
 
