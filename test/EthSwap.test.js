9c561e74f63c67b5b1de211fa7ddd956598322e4
Louie <alouiseq@gmail.com>
2022-07-17 17:02:25 -0700

    add before hook for tests

--- test/EthSwap.test.js
+++ test/EthSwap.test.js
@@ -5,12 +5,34 @@ require('chai')
   .use(require('chai-as-promised'))
   .should()
 
+const convertTokens = n => web3.utils.toWei(n, 'ether');
+
 contract('EthSwap', (accounts) => {
+  let token, ethSwap ;
+  const oneMil = '1000000';
+
+  before(async () => {
+    token = await Token.new();
+    ethSwap = await EthSwap.new();
+    await token.transfer(ethSwap.address, convertTokens(oneMil));
+  })
+
+  describe('Token deployment', async () => {
+    it('contract has a name', async () => {
+      const name = await token.name();
+      assert.equal(name, 'DApp Token');
+    })
+  })
+
   describe('EthSwap deployment', async () => {
     it('contract has a name', async () => {
-      const ethSwap = await EthSwap.new();
       const name = await ethSwap.name(); 
       assert.equal(name, 'EthSwap Instant Exchange');
-    });
-  });
-});
+    })
+
+    it('contract has tokens', async () => {
+      const balance = await token.balanceOf(ethSwap.address);
+      assert.equal(balance.toString(), convertTokens(oneMil));
+    })
+  })
+})
