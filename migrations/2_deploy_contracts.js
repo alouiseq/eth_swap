eb59487630c9e5eacb56205ec8e2f3eefb5fa097
Louie <alouiseq@gmail.com>
2022-07-24 15:01:50 -0700

    buy tokens and write some tests

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -5,7 +5,7 @@ module.exports = async function(deployer) {
   await deployer.deploy(Token);
   token = await Token.deployed();
 
-  await deployer.deploy(EthSwap);
+  await deployer.deploy(EthSwap, token.address);
   ethSwap = await EthSwap.deployed();
 
   await token.transfer(ethSwap.address, '1000000000000000000000000');
--- test/EthSwap.test.js
+++ test/EthSwap.test.js
@@ -7,13 +7,13 @@ require('chai')
 
 const convertTokens = n => web3.utils.toWei(n, 'ether');
 
-contract('EthSwap', (accounts) => {
+contract('EthSwap', ([deployer, investor]) => {
   let token, ethSwap ;
   const oneMil = '1000000';
 
   before(async () => {
     token = await Token.new();
-    ethSwap = await EthSwap.new();
+    ethSwap = await EthSwap.new(token.address);
     await token.transfer(ethSwap.address, convertTokens(oneMil));
   })
 
@@ -33,6 +33,24 @@ contract('EthSwap', (accounts) => {
     it('contract has tokens', async () => {
       const balance = await token.balanceOf(ethSwap.address);
       assert.equal(balance.toString(), convertTokens(oneMil));
+    })
+  })
+
+  describe('buyTokens()', async () => {
+    let result;
+
+    before(async () => {
+      result = await ethSwap.buyTokens({ from: investor, value: web3.utils.toWei('1', 'ether') });
+    })
+
+    it('allows user to instantly purchase tokens at a fixed price from ethSwap', async () => {
+      const investorBalance = await token.balanceOf(investor);
+      assert.equal(investorBalance.toString(), convertTokens('100'));
+
+      const ethSwapTokenBalance = await token.balanceOf(ethSwap.address);
+      assert.equal(ethSwapTokenBalance.toString(), convertTokens('999900'));
+      const ethSwapEthBalance = await web3.eth.getBalance(ethSwap.address);
+      assert.equal(ethSwapEthBalance.toString(), convertTokens('1'));
     })
   })
 })
